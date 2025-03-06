003b117a8ccaa9c7c948cac4838ed55598d5aa04
Mehul Nawal <your.email@example.com>
2025-03-06 13:04:01 +0530

    men and women html change

--- assets/js/men.js
+++ assets/js/men.js
@@ -1,13 +1,13 @@
 // vests
 $(document).ready(function () {
-    // Open shopLook on container click
     $(document).on("click", ".plus-1", function () {
         $(".shopLook-main .shopLook").addClass("shopLook-toggle");
+        $(".prime").css("opacity", "0.6");
     });
 
-    // Close shopLook on cross-icon click
     $(document).on("click", ".shopLook-main .cross-icon", function () {
         $(".shopLook-main .shopLook").removeClass("shopLook-toggle");
+        $(".prime").css("opacity", "1");
     });
 });
 
@@ -16,10 +16,12 @@ $(document).ready(function () {
     // Open shopLook on container click
     $(document).on("click", ".plus-2", function () {
         $(".shopLook-main-shorts .shopLook").addClass("shopLook-toggle");
+        $(".prime").css("opacity", "0.6");
     });
 
     // Close shopLook on cross-icon click
     $(document).on("click", ".shopLook-main-shorts .cross-icon", function () {
         $(".shopLook-main-shorts .shopLook").removeClass("shopLook-toggle");
+        $(".prime").css("opacity", "1");
     });
 });
