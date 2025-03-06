d19a16f59dbf459abc2af248465f5bc2a3a62cac
Mehul Nawal <your.email@example.com>
2025-03-07 00:42:35 +0530

    changes in men.html, women.html, Mne_shorts.html

--- assets/js/men.js
+++ assets/js/men.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
     $(document).on("click", ".plus-1", function () {
         $(".shopLook-main .shopLook").addClass("shopLook-toggle");
         $(".prime").css("opacity", "0.6");
+        $("body").addClass("body-toggle");
     });
 
     $(document).on("click", ".shopLook-main .cross-icon", function () {
         $(".shopLook-main .shopLook").removeClass("shopLook-toggle");
         $(".prime").css("opacity", "1");
+        $("body").removeClass("body-toggle");
     });
 });
 
@@ -17,11 +19,13 @@ $(document).ready(function () {
     $(document).on("click", ".plus-2", function () {
         $(".shopLook-main-shorts .shopLook").addClass("shopLook-toggle");
         $(".prime").css("opacity", "0.6");
+        $("body").addClass("body-toggle");
     });
 
     // Close shopLook on cross-icon click
     $(document).on("click", ".shopLook-main-shorts .cross-icon", function () {
         $(".shopLook-main-shorts .shopLook").removeClass("shopLook-toggle");
         $(".prime").css("opacity", "1");
+        $("body").removeClass("body-toggle");
     });
 });
